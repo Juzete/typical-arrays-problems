89f7b59e69cab1180cc5c81be063fe2034c0a658
Juzete <maximka00007777@gmail.com>
2021-02-10 22:20:04 +0300

    fix: check if no params passed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 exports.min = function min (array) {
-  if(typeof array == undefined) return 0
+  if(arguments.length === 0 || array.length === 0) return 0
   let minimum = array[0]
   for(let i = 1; i < array.length; i++){
     if (minimum > array[i]) minimum = array[i]
@@ -9,7 +9,7 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  if(typeof array == undefined) return 0
+  if(arguments.length === 0 || array.length === 0) return 0
   let maximum = array[0]
   for(let i = 1; i < array.length; i++){
     if (maximum < array[i]) maximum = array[i]
@@ -18,7 +18,7 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  if(typeof array == undefined) return 0
+  if(arguments.length === 0 || array.length === 0) return 0
   let average = 0
   for(let i = 0; i < array.length; i++){
     average+= array[i]
